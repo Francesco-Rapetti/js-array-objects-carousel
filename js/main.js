e6f29c9eecc9382aa5efe21d8f99e52a0eade188
Francesco Rapetti <rapettif80@yahoo.it>
2023-11-02 17:00:29 +0100

    Add images array

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const container = document.getElementById('img-container');
+const arrayContainer = document.getElementById('array-container');
+const upBtn = document.getElementById('up');
+const downBtn = document.getElementById('down');
 const images = [
     "img/01.webp",
     "img/02.webp",
@@ -5,4 +9,11 @@ const images = [
     "img/04.webp",
     "img/05.webp"
 ]
+
+let selected;
+arrayContainer.innerHTML = `<div class="preview"><img src="${images[images.length-1]}"></div>` + arrayContainer.innerHTML;
+for (let i = 0; i < images.length; i++) {
+    arrayContainer.innerHTML += `<div class="preview"><img src="${images[i]}"></div>`;
+}
+arrayContainer.innerHTML += `<div class="preview"><img src="${images[0]}"></div>`;
 
