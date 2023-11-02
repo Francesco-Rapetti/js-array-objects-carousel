cf4dcd38a12f4bf9047306eb074777c94ff51975
Francesco Rapetti <rapettif80@yahoo.it>
2023-11-02 19:56:51 +0100

    feat: add big image preview

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
-// let bottom;
+let currentImg = 2;
 const imgHeight = 107.2;
 const container = document.getElementById('img-container');
 const arrayContainer = document.getElementById('array-container');
@@ -16,11 +16,16 @@ const images = [
 let selected;
 arrayContainer.innerHTML = `<div class="preview"><img src="${images[images.length-1]}"><div class="filter"></div></div>` + arrayContainer.innerHTML;
 for (let i = 0; i < images.length; i++) {
+    container.innerHTML += `<img class="position-absolute" src="${images[i]}">`
     arrayContainer.innerHTML += `<div class="preview"><img src="${images[i]}"><div class="filter"></div></div>`;
 }
 arrayContainer.innerHTML += `<div class="preview"><img src="${images[0]}"><div class="filter"></div></div>`;
 const previews = document.getElementsByClassName('preview');
+const bigImgs = document.querySelectorAll('#img-container > *');
+console.log(bigImgs)
 previews[3].classList.add('no-filter');
+bigImgs[currentImg].style.opacity = 1;
+
 
 upBtn.addEventListener("click", function() {
     console.log('bottone premuto');
@@ -36,6 +41,12 @@ const shiftCardsDown = async (previews) => {
     downBtn.classList.add('disabled');
     previews[3].classList.remove('no-filter');
     previews[4].classList.add('no-filter');
+    bigImgs[currentImg].style.opacity = 0;
+    currentImg++;
+    if (currentImg >= bigImgs.length) {
+        currentImg = 0;
+    }
+    bigImgs[currentImg].style.opacity = 1;
     let bottom = imgHeight * 2;
     for (let i = 0; i < previews.length; i++) {
         previews[i].style.bottom = `${bottom}px`;
@@ -56,6 +67,12 @@ const shiftCardsUp = async (previews) => {
     upBtn.classList.add('disabled');
     previews[3].classList.remove('no-filter');
     previews[2].classList.add('no-filter');
+    bigImgs[currentImg].style.opacity = 0;
+    currentImg--;
+    if (currentImg < 0) {
+        currentImg = bigImgs.length-1;
+    }
+    bigImgs[currentImg].style.opacity = 1;
     let bottom = 0;
     for (let i = 0; i < previews.length; i++) {
         previews[i].style.bottom = `${bottom}px`;
